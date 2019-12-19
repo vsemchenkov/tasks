e0ed97ca5a46a0a8f4d04dd264e7549a65fe2fa3
macbookpro <semchenkovvova@gmail.com>
2019-12-19 18:51:34 +0300

    Tasks -  Fixed

--- task.js
+++ task.js
@@ -1,19 +1,17 @@
 
-// входной массив
-let mas = [10, 6, 5, 12, 25, 99];
-// шаг массива 2
-let k = 2;
+    // входной массив
+    let mas = [10, 6, 5, 12, 25, 99];
+    // шаг массива 2
+    let leng = mas.length;
+    let k = 2;
 
-for (let i = 0; i<k; i++) {
-    mas.unshift(mas.pop());
-}
-
-console.log(mas);
-
-// или
-
-let mas2 = [10, 6, 5, 12, 25, 99];
-
-mas2 = mas2.splice(-k).concat(mas2);
-
-console.log(mas2);
+    for (let i = leng - 1; i >= 0; i--) {
+        mas[i + k] = mas[i];
+    }
+    for (let i = 0; i < k; i++) {
+        mas[i] = mas[mas.length-i-1];
+    }
+    for (let i = k; i > 0; i--) {
+        mas.length--;
+    }
+    console.log(mas);
